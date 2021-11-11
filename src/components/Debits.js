761f50e8731121c35cba85ef65785c9a4a3bed39
evanegas121 <Elizabeth.Vanegas11@myhunter.cuny.edu>
2021-11-11 16:04:46 -0500

    input values

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,3 +1,19 @@
+import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+
+// const handleChange = (e) => {
+//     console.log(e.target.value)
+// }
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    var inputs = document.getElementsByTagName('input');
+    var description = inputs[0].value
+    var amount = inputs[1].value
+    console.log(description,amount)
+    this.useState.addDebit(this.state.debitsView)
+
+}
 
 const Debits=(props) =>{
     let debitsView = () =>{
@@ -7,11 +23,34 @@ const Debits=(props) =>{
             return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
         })
     }
+
     return(
         <div>
-            <h1>Debits</h1>
+             <Link to="/">Return to Home</Link>
+          <center><h1>Debits</h1>
             {debitsView()}
+            <br></br>
+    <form onSubmit = {handleSubmit}>
+     <label>
+        Description
+        <input
+          type="text"
+          name="Description"
+       />
+      </label> 
+      <label>
+        Amount
+           <input
+          type="text"
+           name="Amount"
+        />
+      </label>
+      <button >Add Debits</button>
+    </form>
+            </center>
+
         </div>
+        
     )
 }
 
