d7e434d460faf5b6caf4758ab730a17c7d1787cb
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-11-16 11:26:20 -0500

    Add background and image to user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,16 +2,18 @@
 
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import './UserProfile.css';
 
 class UserProfile extends Component {
   render() {
     return (
         <>
-        <center>
-          <h1>User Profile</h1>
+        <center style={{backgroundColor: 'rgb(100, 200, 0)'}}>
+          <h1> User Profile</h1>
 
           <div>Username: {this.props.userName}</div>
           <div>Member Since: {this.props.memberSince}</div>
+          <img src="https://www.pngitem.com/pimgs/m/75-753817_transparent-customer-icon-png-remarketing-icons-png-download.png" alt="user" width="240" height="300"/>
           <Link to="/">Return to Home</Link>
           </center>
         </>
