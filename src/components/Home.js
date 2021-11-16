3f87bc9b0ee3b268e881f66f3a3628a1e47adb70
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-11-16 11:22:45 -0500

    Add image to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
+import './Home.css';
 
 
 class Home extends Component {
   render() {
     return (
         <div style={{backgroundColor: '#82CAFF'}}>
-          <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
+          <center>
           <h1 style= {{"text-align": "center"}}>Bank of React</h1>
-          <p style= {{"text-align": "center"}}><Link to="/userProfile">User Profile</Link> &nbsp;&nbsp;<Link to="/LogIn">Login</Link> 
+         <center> <img src="https://d28wu8o6itv89t.cloudfront.net/images/bankjpg-1591875524478.jpeg" alt="bank"/></center>
+          <p className = "links" style= {{"text-align": "center"}}><Link to="/userProfile">User Profile</Link> &nbsp;&nbsp;<Link to="/LogIn">Login</Link> 
           &nbsp;&nbsp;<Link to="/Debits">Debit</Link>&nbsp;&nbsp;<Link to="/Credits">Credit</Link></p>
-        
-          <AccountBalance accountBalance={this.props.accountBalance}/>
+         <div className="Balance" >
+         <AccountBalance accountBalance={this.props.accountBalance}/>
+         </div></center>
         </div>
     );
   }
