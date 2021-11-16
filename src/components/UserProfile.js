9f39d50a4ca41769e4529bf1ff3e34cc775b7da1
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2021-11-16 12:43:42 -0500

    Update UserProfile.js
    Center link to home

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -14,7 +14,7 @@ class UserProfile extends Component {
           <div>Username: {this.props.userName}</div>
           <div>Member Since: {this.props.memberSince}</div>
           <img src="https://www.pngitem.com/pimgs/m/75-753817_transparent-customer-icon-png-remarketing-icons-png-download.png" alt="user" width="240" height="300"/>
-          <Link to="/">Return to Home</Link>
+          <div><Link to="/">Return to Home</Link></div>
           </center>
         </>
     );
