cdece84071ae6aad8f2f4e3314cd529de75cf4bc
lishannon <lishannon490@gmail.com>
2021-11-17 20:56:51 -0500

    Adding deploying stuff

--- src/App.js
+++ src/App.js
@@ -107,12 +107,12 @@ class App extends Component {
     return (
         <Router>
           <div>
-            <Route exact path="/" render={HomeComponent}/>
-            <Route exact path="/userProfile" render={UserProfileComponent}/>
-            <Route exact path="/login" render={LogInComponent}/> 
+            <Route exact path="/Bank-of-React" render={HomeComponent}/>
+            <Route exact path="/Bank-of-React/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/Bank-of-React/login" render={LogInComponent}/> 
             <switch>
-            <Route exact path="/debits" render={DebitsComponent}/> 
-            <Route exact path="/credits" render={CreditsComponent}/> 
+            <Route exact path="/Bank-of-React/debits" render={DebitsComponent}/> 
+            <Route exact path="/Bank-of-React/credits" render={CreditsComponent}/> 
             </switch>
           </div>
         </Router>
