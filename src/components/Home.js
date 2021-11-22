a8e342f5074bb93952b7119ef9757d13ffc0b00f
lishannon <lishannon490@gmail.com>
2021-11-21 21:07:53 -0500

    Code clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-        <div style={{backgroundColor: '#F0F8FF'}}>
+        <div style={{backgroundColor: '#82CAFF'}}>
           <h1 style= {{"text-align": "center"}}>Bank of React</h1>
           <p style= {{"text-align": "center"}}><Link to="/Bank-of-React/userProfile">User Profile</Link> &nbsp;&nbsp;<Link to="/Bank-of-React/LogIn">Login</Link> 
           &nbsp;&nbsp;<Link to="/Bank-of-React/Debits">Debit</Link>&nbsp;&nbsp;<Link to="/Bank-of-React/Credits">Credit</Link></p>
