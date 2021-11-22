eaa9693d050a1475fe23e567b5ffd626601329f3
lishannon <lishannon490@gmail.com>
2021-11-21 20:52:18 -0500

    clean up the code

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,22 +19,4 @@ class Home extends Component {
 }
 
 export default Home;
-// // src/components/Home.js
 
-// import React, {Component} from 'react';
-// import AccountBalance from './AccountBalance';
-
-// class Home extends Component {
-//   render() {
-//     return (
-//         <div>
-//           <img src="https://picsum.photos/201" alt="bank"/>
-//           <h1>Bank of React</h1>
-          
-//           <AccountBalance accountBalance={this.props.accountBalance}/>
-//         </div>
-//     );
-//   }
-// }
-
-// export default Home;
